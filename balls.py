4cd97f043ab1bcbd6e15f7ba0ec7d65024df5ff1
Liza-T <lizztriff@mail.ru>
2019-11-04 10:15:15 +0300

    pep8

--- balls.py
+++ balls.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 from random import randrange as rnd, choice
-import time
 import math
 
 root = tk.Tk()
 root.geometry('800x600')
-canv = tk.Canvas(root, bg = '#7ebfc3')
-canv.pack(fill = tk.BOTH, expand = 1)
+canv = tk.Canvas(root, bg='#7ebfc3')
+canv.pack(fill=tk.BOTH, expand=1)
 
 colors2 = ['#b5dfe5', '#437f83', '#a0d2d8', '#5e9fa3', '#d2f1f6']
 colors1 = ['#ae215b', '#c1317e']
@@ -17,22 +16,27 @@ balls_number2 = 10
 
 
 class Ball():
-    
+
     def __init__(self, r, color):
         self.x = rnd(0 + r, 800 - r)
         self.y = rnd(0 + r, 600 - r)
         self.r = r
         self.color = color
-        self.obj = canv.create_oval(self.x - r, self.y - r, \
-                                    self.x + r, self.y + r, \
-                                        fill = color, width = 0)
+        self.obj = canv.create_oval(
+                        self.x - r, self.y - r, 
+                        self.x + r, self.y + r, 
+                        fill=color, width=0
+                        )
 
     def delete(self):
         canv.delete(self.obj)
 
     def draw_ball(self):
-        self.obj = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, \
-            self.y + self.r, fill = self.color, width = 0)
+        self.obj = canv.create_oval(
+                        self.x - self.r, self.y - self.r,
+                        self.x + self.r, self.y + self.r,
+                        fill=self.color, width=0
+                        )
 
     def reflection(self):
         if self.x - self.r < 0 and self.vx < 0:
@@ -48,19 +52,19 @@ class Ball():
             self.y -= (self.y + self.r - 600+20)
             self.vy = -self.vy
 
-
     def collision(self, other):
-            dist = ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
-            m1 = self.r 
+            dist = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+            m1 = self.r
             m2 = other.r
             if abs(self.x - other.x)/dist >= 1:
-                fi = 0 
-            elif self.x - other.x > 0 and self.y - other.y > 0 \
-                 or self.x - other.x < 0 and self.y - other.y < 0:
-                fi = math.acos(abs(self.x - other.x)/dist) 
+                fi = 0
+            elif (self.x - other.x > 0
+                    and self.y - other.y > 0 
+                    or self.x - other.x < 0
+                    and self.y - other.y < 0):
+                fi = math.acos(abs(self.x - other.x) / dist)
             else:
                 fi = math.pi - math.acos(abs(self.x - other.x)/dist)
-
             v01 = (self.vx ** 2 + self.vy ** 2) ** 0.5
             v02 = (other.vx ** 2 + other.vy ** 2) ** 0.5
             angle1 = math.acos(self.vx / v01)
@@ -79,10 +83,14 @@ class Ball():
             vy2 = v02 * math.sin(a2)
             v1 = (vx1 ** 2 + vy1 ** 2) ** 0.5
             v2 = (vx2 ** 2 + vy2 ** 2) ** 0.5
-            if vy1 !=0: a1 = math.acos(vx1 / v1) * vy1/abs(vy1)
-            else: a1 = math.acos(vx1 / v1)
-            if vy2 != 0: a2 = math.acos(vx2 / v2) * vy2/abs(vy2)
-            else: a2 = math.acos(vx2 / v2)
+            if vy1 !=0:
+                a1 = math.acos(vx1 / v1) * vy1 / abs(vy1)
+            else:
+                a1 = math.acos(vx1 / v1)
+            if vy2 != 0:
+                a2 = math.acos(vx2 / v2) * vy2 / abs(vy2)
+            else:
+                a2 = math.acos(vx2 / v2)
             angle11 = (a1 + fi) % (math.pi * 2)
             angle22 = (a2 + fi) % (math.pi * 2)
             self.vx = v1 * math.cos(angle11)
@@ -94,11 +102,10 @@ class Ball():
             self.y += r * math.sin(fi)
             other.x -= r * math.cos(fi)
             other.y -= r * math.sin(fi)
-        
 
 
 class Ball1(Ball):
-    
+
     def __init__(self, r, color):
         super().__init__(r, color)
         self.vx = rnd(0, 60)/20
@@ -116,7 +123,7 @@ class Ball1(Ball):
 
 
 class Ball2(Ball):
-    
+
     def __init__(self, r, color):
         super().__init__(r, color)
         self.vx = rnd(0, 40)/20
@@ -134,7 +141,9 @@ def click(event):
     global score, l
     k = -1
     for i in range(len(balls)):
-        if ((balls[i].x - event.x) ** 2 + (balls[i].y - event.y) ** 2) ** (1 / 2) <= balls[i].r:
+        if (
+        ((balls[i].x - event.x) ** 2 +
+        (balls[i].y - event.y) ** 2) ** (1 / 2)<= balls[i].r):
             k = i
     if k >= 0:
         canv.delete(balls[k].obj)
@@ -153,7 +162,7 @@ def click(event):
             balls[k].vx = rnd(0, 60) / 20
             balls[k].vy = rnd(0, 60) / 20
         balls[k].draw_ball()
-        l['text'] = str(score)
+        label['text'] = str(score)
 
 
 def move_all_balls():
@@ -163,7 +172,8 @@ def move_all_balls():
         else:
             balls[i].move_ball1()
         for j in range(len(balls)):
-            dist = ((balls[i].x - balls[j].x) ** 2 + (balls[i].y - balls[j].y) ** 2) ** 0.5
+            dist = ((balls[i].x - balls[j].x) ** 2 +
+                    (balls[i].y - balls[j].y) ** 2) ** 0.5
             if i != j and dist < (balls[i].r + balls[j].r):
                 balls[i].collision(balls[j])
     root.after(delta_t, move_all_balls)
@@ -172,15 +182,14 @@ def move_all_balls():
 def write():
     global score, name
     scores = []
-    list1 = []
     with open('table.txt', 'w') as f:
         pass
     with open('table.txt', 'r') as f:
         for i in f:
             scores.append(i.split(',  '))
         scores.append([name, str(score) + '\n'])
-        scores.sort(key = lambda s: int(s[1][0:len(s[1]) - 1]), reverse = True)
-    with open('table.txt', 'w') as f:    
+        scores.sort(key=lambda s: int(s[1][0:len(s[1]) - 1]), reverse=True)
+    with open('table.txt', 'w') as f:
         for i in scores:
             f.write(',  '.join(i))
 
@@ -188,8 +197,8 @@ def write():
 balls = []
 print('What\'s your name?')
 name = input()
-l = tk.Label(root, bg = 'white', fg = 'black', width=20)
-l.pack()
+label = tk.Label(root, bg='white', fg='black', width=20)
+label.pack()
 for i in range(balls_number1):
     b = Ball1(rnd(7, 15), choice(colors1))
     balls.append(b)
